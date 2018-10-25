2b542c16bad5cb8edc43c3627a9b4d9ca9da144c
Manu <10000189.joan23@fje.edu>
2018-10-25 18:04:38 +0200

    header y footer terminado

---
+++ WEB/PHP/footer.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<link href="footer.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<footer>
+			<div class="footer">
+				<h3>Pagina web de reservas</h3>
+				<p>Esta pagina ha sido diseñada y desarollada por Walter White, un equipo del grupo de clase DAW2 formado por Adria Soto, Marc Lopez y Manuel Carceller en la elaboracion de su primer proyecto</p>
+				
+		</div>
+		<div class="linea">
+					<h3>Walter white</h3>
+			</div>
+		</footer>
+	</body>
+</html>
--- WEB/PHP/header.php
+++ WEB/PHP/header.php
@@ -1,4 +1,37 @@
-<?php
-	include 'conection.php';
+<html>
+<head>
+	<link href="headcss.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<nav>
+	<div class="todo">
+		<div class="imagen">
+			<br>
+			<img src="prueba.jpg"width="110" height="40"/>
+		</div>
+		<a href='Misreservas.php'>Mis Reservas</a>
+		<a href='Reservas.php'>Reservas</a>
+		<div class="menuCSS3">
+        <ul>
+            </li>
+            <li><a href="#">Hola
+            	<?php
+            	$_SESSION['nombre']="manu";
+				echo $_SESSION['nombre'];
+				?> 
+				</a>
+                <ul>
+                    <li><a href="#">Cerrar sesion</a></li>
+                </ul>
 
-?>
+            </li>
+        </ul>
+    </div
+			</li>
+		</ul>
+	</div>
+		</div>
+	</div>
+	</nav>
+</body>
+</html>
