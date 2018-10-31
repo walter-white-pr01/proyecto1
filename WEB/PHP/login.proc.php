34755ace166ec9c362d3b93c56f98ae9f464f3f9
TekunoMaku <tekunomaku@gmail.com>
2018-10-31 19:35:31 +0100

    index redirige a misreservas.php

--- WEB/PHP/login.proc.php
+++ WEB/PHP/login.proc.php
@@ -18,7 +18,7 @@ if (mysqli_num_rows($q_usuarios)>0) {
 	$array = mysqli_fetch_array($q_usuarios);
 	$_SESSION['nombre']=$array['user_name'];
 	$_SESSION['id']=$array['id_user'];
-	header('Location: reservar.php');
+	header('Location: misreservas.php');
 }else{
 	header('Location: index.php?err=1');
 }
